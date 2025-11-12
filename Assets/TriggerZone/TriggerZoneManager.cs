cc1a02e85bad9ea262dbee85b49b4ad5272b29e3
Patrick Alexander <118601385+Seryxss@users.noreply.github.com>
2025-11-12 21:44:51 +0800

    Direction Arrow
    Tambah di script TriggerZoneManager - punya kak mahesa

--- Assets/TriggerZone/TriggerZoneManager.cs
+++ Assets/TriggerZone/TriggerZoneManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class TriggerZoneManager : MonoBehaviour
 {
     private List<GameObject> _triggerZones = new List<GameObject>();
+    public Transform target; // The player
+    public Transform arrowTransform; // The arrow GameObject
+    public float rotationOffsetY = -45f; // Y-axis offset only
 
-    // Start is called before the first frame update
     void Start()
     {
         InitTriggerZonesList();
@@ -27,14 +29,36 @@ public class TriggerZoneManager : MonoBehaviour
         _triggerZones.Remove(triggerZone.gameObject);
         Destroy(triggerZone.gameObject);
         Debug.Log("Paket/Rumah Tersisa: " + _triggerZones.Count);
+        
         if (_triggerZones.Count == 0) {
             Debug.Log("Semua Paket/Rumah Telah Selesai");
+            if (arrowTransform != null) {
+                arrowTransform.gameObject.SetActive(false);
+            }
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (_triggerZones.Count > 0 && _triggerZones[0] != null && arrowTransform != null)
+        {
+            PointArrowToCurrentTarget();
+        }
+    }
+
+    private void PointArrowToCurrentTarget()
+    {
+        GameObject currentTarget = _triggerZones[0];
         
+        if (currentTarget == null) return;
+
+        Vector3 relativePos = currentTarget.transform.position - arrowTransform.position;
+        relativePos.y = 0;
+        
+        if (relativePos != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(relativePos.x, relativePos.z) * Mathf.Rad2Deg;
+            arrowTransform.rotation = Quaternion.Euler(0, angle + rotationOffsetY, 0);
+        }
     }
 }
